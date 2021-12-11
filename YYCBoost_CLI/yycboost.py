d37e2242d2fc8e0f806b063f5e4437e049ed3d80
Joseph Crowe <josephjccrowe@gmail.com>
2021-12-11 07:22:07 +0100

    WIP: Add /help, /background, /close, /auto, and -buildpath options

--- YYCBoost_CLI/yycboost.py
+++ YYCBoost_CLI/yycboost.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 import json
 import os
+import os.path
 import signal
 import sys
 import time
+import subprocess
+from subprocess import DEVNULL, CREATE_NO_WINDOW
 from multiprocessing import Process, Queue, cpu_count, freeze_support
+from argparse import ArgumentParser
 
 from termcolor import cprint
 from watchdog.events import FileSystemEventHandler
@@ -83,9 +87,98 @@ class EventHandler(FileSystemEventHandler):
                 return
         queue.put((fpath, build,))
 
+def parse_args(*args, **kwds):
+    # Apply some cosmetic changes to the help and usage output:
+    class CustomArgumentParser(ArgumentParser):
+        def format_usage(self):
+            usage = super(CustomArgumentParser, self).format_usage()
+            return usage.replace('usage: ', 'Usage: ')
+
+        def format_help(self):
+            help = super(CustomArgumentParser, self).format_help()
+            return help.replace('usage: ', 'Usage: ') \
+                       .replace('buildpath PATH', 'buildpath=PATH')
+
+    arg_parser = CustomArgumentParser(
+        prefix_chars='/-', allow_abbrev=False, add_help=False)
+
+    main_args = arg_parser.add_argument_group('Options')
+    main_args.add_argument(
+        '/?', '/help', action='help',
+        help='Show information about command-line arguments.',
+    )
+    main_args.add_argument(
+        '/b', '/background', dest='run_in_background', action='store_true',
+        help='Create a background process that is detached from the current '
+             'terminal (if any) and allows the calling process to continue '
+             'immediately instead of blocking until finished, and closes after '
+             'the first build. Implies "/close" and "/auto".'
+    )
+    main_args.add_argument(
+        '/c', '/close', dest='close_after_first_build', action='store_true',
+        help='Close the yycboost process after the first build; otherwise, '
+             'it stays open until manually closed.',
+    )
+    main_args.add_argument(
+        '/a', '/auto', dest='auto', action='store_true',
+        help='Use default values rather than prompting the user for any input.',
+    )
+    main_args.add_argument(
+        '-buildpath', dest='build_bff_path', nargs=1, metavar='PATH',
+        help='The path of the "build.bff" file corresponding to the target. '
+             'If not specified, the user is prompted to enter a path on the '
+             'terminal, unless "/auto" is set, in which case the '
+             'default value of "{}" is used.'.format(BuildBff.PATH_DEFAULT),
+    )
+
+    other_args = arg_parser.add_argument_group('Positional arguments')
+    other_args.add_argument(
+        'cpp_dir', metavar='CPP_DIR', nargs='?', help='Load cache directory '
+        'from command line (no injection, only cleanup, handy for GMS1.4) '
+        '[Note: this option doesn\'t appear to do anything useful; it has '
+        'been kept for backward compatibility, but may be removed in the '
+        'future. The "cache directory" referred to here is the project-'
+        'specific directory for .gml.cpp files, for example "%%LocalAppData%%\\'
+        'GameMakerStudio2\\Cache\\GMS2CACHE\\YYCBoost_2083F10D\\YYCBoost\\'
+        'Default\Scripts\llvm-win"; if this argument is present, then the '
+        '"build.bff" file is not loaded and its path need not be given.]',
+    )
+
+    return arg_parser.parse_args(*args, **kwds)
+
 
 if __name__ == "__main__":
     freeze_support()
+
+    args = parse_args()
+    if args.run_in_background:
+        abs_argv0 = os.path.join(os.getcwd(), sys.argv[0])
+        if os.path.samefile(abs_argv0, sys.executable):
+            # Guess we are running as a standalone executable file.
+            new_argv = [abs_argv0] + sys.argv[1:]
+        elif '__file__' in globals() and os.path.samefile(sys.argv[0], __file__):
+            # Guess we are running as a script under an executable interpreter.
+            new_argv = [sys.executable] + sys.argv
+        else:
+            # Unable to determine correct command line to relaunch self.
+            print('Error: failed to launch background process!')
+            sys.exit(1)
+
+        # Ensure that "/close" and "/auto" are set and "/background" is not
+        # set for the background process:
+        new_argv = [arg for arg in new_argv if arg not in ('/b', '/background')]
+        if all(arg not in new_argv for arg in ('/c', '/close')):
+            new_argv.append('/close')
+        if all(arg not in new_argv for arg in ('/a', '/auto')):
+            new_argv.append('/auto')
+
+        proc = subprocess.Popen(new_argv,
+            stdin=DEVNULL, stdout=DEVNULL, stderr=DEVNULL,
+            creationflags=CREATE_NO_WINDOW, start_new_session=True
+        )
+        print('Continuing in background as PID {} with command line "{}".'
+              .format(proc.pid, ' '.join(new_argv)))
+        sys.exit(0)
 
     if getattr(sys, "frozen", False):
         yyc_boost_dir = os.path.dirname(sys.executable)
@@ -95,27 +188,35 @@ if __name__ == "__main__":
     config_fname = "config.json"
     config_fpath = os.path.join(yyc_boost_dir, config_fname)
 
-    if len(sys.argv) > 1:
+    if args.cpp_dir is not None:
         # Load cache directory from command line (no injection, only cleanup,
         # handy for GMS1.4)
+
+        # This doesn't seem to do anything except print errors at the moment;
+        # what is the "cleanup" referred to in the above comment? Should this be
+        # removed?
+
         build = None
-        path_cpp = sys.argv[1]
+        path_cpp = args.cpp_dir
     else:
         # Config
         config = {}
+
         default = BuildBff.PATH_DEFAULT
-        build_bff_dir = input(
-            "Enter path to the build.bff file [{}]: ".format(default))
-        if not build_bff_dir:
-            build_bff_dir = default
-        config["build_bff"] = build_bff_dir
+        [build_bff_path] = args.build_bff_path or [None]
+        if not build_bff_path and not args.auto:
+            build_bff_path = input(
+                "Enter path to the build.bff file [{}]: ".format(default))
+        if not build_bff_path:
+            build_bff_path = default
+        config["build_bff"] = build_bff_path
 
         # Load build.bff
         try:
-            build = BuildBff(build_bff_dir)
+            build = BuildBff(build_bff_path)
             print("Loaded build.bff")
         except:
-            print("ERROR: Could not load build.bff!")
+            print('ERROR: Could not load "{}"!'.format(build_bff_path))
             sys.exit(1)
 
         # Project info
@@ -127,9 +228,10 @@ if __name__ == "__main__":
     print("Target directory:", path_cpp)
 
     # Check for permission
-    if input("Do you really want to modify the files? [Y/n] ") == "n":
-        cprint("Canceled", "magenta")
-        sys.exit(0)
+    if not args.auto:
+        if input("Do you really want to modify the files? [Y/n] ") == "n":
+            cprint("Canceled", "magenta")
+            sys.exit(0)
 
     # Copy custom headers
     _cpp_dir = os.path.join(yyc_boost_dir, "cpp")
